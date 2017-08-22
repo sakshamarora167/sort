ab052027eea4d7c7ffd51abf27639f68fc047635
saksham.arora <saksham.arora@CSELAB02-06.bennett.edu.in>
2017-08-22 13:06:51 +0530

    here it is!!

--- sort/src/sort/Sort.java
+++ sort/src/sort/Sort.java
@@ -1,9 +1,34 @@
 package sort;
 
-public class Bubble_sort {
 
-	public static void main(String[] args) {
-	
-	}
+import java.util.Scanner;
 
+public class Sort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("input the size of list : ");
+        int size = s.nextInt();
+        int dyn_size=size;
+        int list[] = new int[size];
+        for(int i=0;i<size;i++){
+            System.out.println("give the number");
+            list[i]=s.nextInt();
+        }
+        for(int i=0;i<size;i++){
+            for (int j=0;j<dyn_size-1;j++){
+                if(list[j]>list[j+1]){
+                    int temp=list[j+1];
+                    list[j+1]=list[j];
+                    list[j]=temp;
+                }
+            }
+            dyn_size-=i;
+        }
+        System.out.println(java.util.Arrays.toString(list));
+    }
+    
 }
